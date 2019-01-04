ce744461a67210dd8c23c067febccb210bfafb46
sstefan1 <sstipanovic@s-energize.com>
2019-01-04 13:19:50 +0100

    create torrent

--- bencode-test.py
+++ bencode-test.py
@@ -4,30 +4,33 @@ import bencode, hashlib, os
 def create_torrent(tracker, file):
     be_dict = {'announce': tracker}
 
-    be = bencode.encode(be_dict)
+    name = file.split("\\")[-1]
     size = os.path.getsize(file)
-    with open(file) as file:
-        while True:
-            data = file.read()
+
+    # Default piece length is 512kB.
+    piece_length = 2**9
+    pieces = sha1_file(file, piece_length)
+
+    # info dictionary.
+    info_dict = {'length': size, 'name': name, 'piece_length': piece_length, 'pieces': pieces}
+    be_dict['info'] = info_dict
+
+    be = bencode.encode(be_dict)
 
     torrent_path = file + ".torrent"
     with open(torrent_path, 'a') as torrent_file:
         torrent_file.write(be.decode("utf-8"))
 
 
-def sha1_file(filepath):
-    BUF_SIZE = 65536
-
-    md5 = hashlib.md5()
-    sha1 = hashlib.sha1()
-
-    with open(r"C:\Users\stefan\Desktop\book.pdf", 'rb') as f:
+def sha1_file(filepath, BUFF_SIZE):
+    result = []
+    with open(filepath, 'rb') as f:
         while True:
             data = f.read(BUF_SIZE)
             if not data:
                 break;
-            md5.update(data)
-            sha1.update(data)
+            result.append(ihashlib.sha1(data).digest())
 
-# print("MD5: {0}".format(md5.hexdigest()))
-# print("SHA1: {0}".format(sha1.hexdigest()))
+    return ''.join(result)
+
+# with open(r"C:\Users\stefan\Desktop\book.pdf", 'rb') as f:
