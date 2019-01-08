b36ff35144982b2402994e4ca3bb9b4cb84de251
sstefan1 <sstipanovic@s-energize.com>
2019-01-08 15:20:45 +0100

    send handshake

--- torrent/Peer.py
+++ torrent/Peer.py
@@ -1,4 +1,4 @@
-
+import socket
 class Peer:
     def __init__(self, torrent, host, port, peer_id):
         self.torrent = torrent
@@ -19,3 +19,20 @@ class Peer:
     def send_handshake(self):
         peers = self.tracker.peers
 
+        #  construct the handshake message
+        name_length = bytes([19])
+        protocol_name = b'BitTorrent protocol'
+        reserved_flags = b' ' * 8
+
+        info_hash = self.torrent.urlInfoHash
+        peer_id = self.peer_id
+
+        hadnskahe_msg = name_length + protocol_name + info_hash + peer_id
+
+        peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        peer_socket.connect((self.host, self.port))
+        peer_socket.send(handshake_msg)
+
+        #  handshake msg is exactly 68 bytes long
+        response = socket.recv(68)
+        print(response)
