099ba28a58a160ea02fde4dbfc5cd042538df709
Miroslav Tanasic <tanasicmiroslav@gmail.com>
2019-01-10 15:17:42 +0100

    buttons and canvas

--- gui/add_torrent.py
+++ gui/add_torrent.py
@@ -32,3 +32,51 @@ class TorrentAddingDialog(QDialog):
         item.setCheckState(0, Qt.Checked)
         item.setText(0, name)
         if(isinstance(node,
+
+#odvade
+
+# Add a grid
+mainframe = Frame(root)
+mainframe.grid(column=0, row=0)
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+mainframe.pack()
+
+
+canvas = Canvas(root, bg='white')
+canvas.pack(side=BOTTOM, anchor=W)
+
+
+tkvar = StringVar(root)
+
+# Dictionary with options
+choices = {'New', 'Open'}
+
+popupMenu = OptionMenu(root, tkvar, *choices).pack(side=LEFT, anchor=NW)
+
+photo = PhotoImage("C:\\Users\\Mimin\\Desktop\\blok4\\btc-master\\gui\\remove.svg")
+
+a = tk.Button(root, text="Add", fg="red", command=quit)
+#a.config(image=photo, width="35", height="23")
+a.pack(side=LEFT, anchor=NW)
+
+b = tk.Button(root, text="Pause", fg="red", command=quit)
+#b.config(width="1", height="1")
+b.pack(side=LEFT, anchor=NW)
+
+c = tk.Button(root, text="Resume", fg="red", command=quit)
+#c.config(width="1", height="1")
+c.pack(side=LEFT, anchor=NW)
+
+d = tk.Button(root, text="Remove", fg="red", command=quit)
+#d.config(width="1", height="1")
+d.pack(side=LEFT, anchor=NW)
+
+e = tk.Button(root, text="About", fg="red", command=quit)
+#e.config(width="1", height="1")
+e.pack(side=LEFT, anchor=NW)
+
+root.mainloop()
+
+		
+		
